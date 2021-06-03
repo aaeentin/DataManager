a40404294654a74e193f1cec38545d5d7b627226
Antin Brattsev <you@example.com>
2021-06-03 10:37:28 +0300

    hasura connected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloTestComponent } from './apolloTest/apolloTest.component';
+import { FirstComponentComponent } from './firstComponent/firstComponent.component';
+//import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+    ApolloTestComponent,
+    FirstComponentComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GraphQLModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'apollo', component: ApolloTestComponent },
+      { path: 'first', component: FirstComponentComponent }
+    ],
+    { enableTracing: true })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
